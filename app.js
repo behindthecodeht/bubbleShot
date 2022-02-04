72b58cd39b250d6251ca66c42cd6302fd7825936
Louis Midson Lajeanty <louismidsonlajeanty@gmail.com>
2022-02-04 10:27:54 -0500

    Update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-let duration = 10
+let duration = 6
 let time = 1000
 let score = 0;
+let runStop = false
 
 const scoreBox = document.getElementById('score')
 
@@ -9,28 +10,40 @@ const bubbleGenarate = () => {
     bubble.className = "bubble"
     document.getElementById('app').appendChild(bubble)
     
-    const size = Math.random() * 50 + 50 + "px"
-    bubble.style.width = size
-    bubble.style.height = size
+    const size = Math.random() * 50 + 50
+    bubble.style.width = size + "px"
+    bubble.style.height = size + "px"
     
     bubble.style.left = Math.random() * 100 + "%"
-    bubble.style.animationDuration = duration + "s"
+    bubble.style.top = Math.random() * 50 + 50 + "%"
+
+    bubble.style.setProperty('--animD', duration + "s")
+    bubble.style.setProperty('--top', size / 2 + "px")
+    bubble.style.setProperty('--left', Math.random() * 100 + "%")
+
+    let bubbleAlive = setTimeout(() => {
+        stopRun();
+    }, duration * 1000);
 
     bubble.addEventListener('click', (e) => {
+        clearTimeout(bubbleAlive)
         score++
         scoreBox.textContent = score
         e.target.remove()
 
-        if (score % 5 === 0 && time != 300) {
-            time -= 100
-            console.log(time);
-        }
-
-        if (score % 10 === 0 && duration != 5) {
-            duration -= 1
-            console.log(duration);
-        }
+        if (score % 5 === 0 && time > 200) time -= 100
+        if (score % 10 === 0 && duration > 2) duration -= 1
     })
 }
 
-setInterval(bubbleGenarate, time)
+const stopRun = () => {
+    document.querySelectorAll('.bubble').forEach(bubble => bubble.remove())
+    clearInterval(run)
+    if (!runStop) {
+        runStop = true
+        alert('Vous Avez Perdu !! Desolé ...')
+    }
+}
+
+let run = setInterval(bubbleGenarate, time)
+
